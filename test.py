6c189358c832344d7e153f1a1dd278caaeca4fd3
TsukasaTakahashi2 <101648061+TsukasaTakahashi2@users.noreply.github.com>
2022-03-16 03:10:55 +0900

    host change

--- test.py
+++ test.py
@@ -27,4 +27,5 @@ app.layout = html.Div(children=[
 ])
 
 if __name__ == '__main__':
-    application.run(debug=True, host='0.0.0.0', port='80')
+#     application.run(debug=True, host='0.0.0.0', port='80')
+    application.run(host='0.0.0.0')
